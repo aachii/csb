267f986ee441096e678115d2e79c39c7c9701e53
Janick Stucki <stuckijanick@gmail.com>
2018-11-07 21:22:58 +0100

    Update base32enc.asm

--- base32/base32enc.asm
+++ base32/base32enc.asm
@@ -26,34 +26,36 @@ Read:
 	mov eax,Buff		; Offset of Buffer to eax
 	mov ebx,BUFFLEN		; Number of bytes to read to ebx
 	call ReadBuff		; Call ReadBuff
-	mov ebp,eax			; Save # of bytes read from file for later
-	cmp eax,0			; If eax=0, sys_read reached EOF on stdin
-	je Done				; Jump If Equal (to 0, from compare)
+	mov ebp,eax		; Save # of bytes read from file for later
+	cmp eax,0		; If eax=0, sys_read reached EOF on stdin
+	je Done			; Jump If Equal (to 0, from compare)
 
 ; Get 5 bits and translate from Table
 	mov esi,Buff
 	xor ecx,ecx
 	
 Loop:
-	xor eax,eax			; clear eax
-	mov edx,ecx			; store offset in edx
+	xor eax,eax		; clear eax
+	mov edx,ecx		; store offset in edx
 	
 	mov al,byte [esi+edx]	; get byte from buffer with offset
-	and al,1Fh				; mask out the 5 first bits 0001 1111
+	and al,1Fh		; mask out the 5 first bits 0001 1111
 	mov bl,byte [Table+eax]	; get the matching character from data Table
-	mov byte [Res+edx],bl	; store this character in memory (Res)
+	mov eax,ebx		; store this character in memory (Res)
+	mov ebx,8
+	call PrintString
 	
-	add ecx,5			; shift to the next 5 bits
-	cmp ecx,ebp			; compare pointer to buffer
-	jna Loop			; 
+	add ecx,5		; shift to the next 5 bits
+	cmp ecx,ebp		; compare pointer to buffer
+	jna Loop		; 
 	
-; Write the line of hexadecimal values to stdout:
-	mov eax,Res		; Specify sys_write call
-	mov ebx,RESLEN		; Specify File Descriptor 1: Standard output
+; Prints newline to end:
+	mov eax,10		; Specify sys_write call
+	mov ebx,2		; Specify File Descriptor 1: Standard output
 	call PrintString	; call PrintString from io.asm
 	jmp Read		; Loop back and load file buffer again
 
-; All done! Let's end this party:
+; Exit:
 Done:
 	mov eax,1		; Code for Exit Syscall
 	mov ebx,0		; Return a code of zero	
