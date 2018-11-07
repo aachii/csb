6d2947fcc2d9dac5b83008796a6b48aac9e45843
Janick Stucki <stuckijanick@gmail.com>
2018-11-07 20:16:52 +0100

    Update base32enc.asm

--- base32/base32enc.asm
+++ base32/base32enc.asm
@@ -4,50 +4,55 @@
 ;  Description     : Encodes text to base32
 ;
 SECTION .bss			; Section of uninitialised data
-
 	BUFFLEN	equ 5		; read the input 5 bytes
-	Buff: 	resb BUFFLEN	; Text buffer
+	Buff: resb BUFFLEN	; Text buffer
+	RESLEN equ 8		; Result length of 8 bytes
+	Res: resb RESLEN	; Result variable
 	
 SECTION .data			; Section of initialised data
+	Table: db "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567"
+	TABLELEN equ $-Table
+	; 5 bits from input is the offset in data for base32
 	
 SECTION .text			; Section of code
-
+	
 GLOBAL 	_start			; Start point for linker
-
+	
 _start:
-	nop			; This no-op keeps gdb happy...
-
-; Read a buffer full of text from stdin:
+	nop					; This no-op keeps gdb happy...
+	
+; Read a buffer from stdin:
 Read:
-	mov eax,Buff		; Pass offset of the buffer to read to
-	mov ebx,BUFFLEN		; Pass number of bytes to read at one pass
-	call ReadBuff		; Call ReadBuff from io.asm to read Buffer
-	mov ebp,eax		; Save # of bytes read from file for later
-	cmp eax,0		; If eax=0, sys_read reached EOF on stdin
-	je Done			; Jump If Equal (to 0, from compare)
+	mov eax,Buff		; Offset of Buffer to eax
+	mov ebx,BUFFLEN		; Number of bytes to read to ebx
+	call ReadBuff		; Call ReadBuff
+	mov ebp,eax			; Save # of bytes read from file for later
+	cmp eax,0			; If eax=0, sys_read reached EOF on stdin
+	je Done				; Jump If Equal (to 0, from compare)
 
-; Make all UPPERCASE letter lowercase (add 32dec to char-> 20h)
-	mov ebx,Buff		; ebx is Buffer address
-	mov ecx,ebp		; ecx is length of input
-
+; Get 5 bits and translate from Table
+	mov esi,Buff
+	mov r5,5
+	xor ecx,ecx
+	
 Loop:
-	cmp byte [ebx],41h	; compare first byte in ebx to 41h (letter A)
-	jb Skip			; jump to Skip if below A
-	cmp byte [ebx],5Ah	; compare first byte to 5Ah (letter Z)
-	ja Skip			; jump to Skip if above Z
-
-	add byte [ebx],32	; this is only executed if inside A-Z
-				; add 32 to the byte at ebx (UPPERCASE to lowercase, see ASCII table)
-Skip:
-	inc ebx			; ebx +1 for next character
-	dec ecx			; ecx -1 for check if we are done
-	jnz Loop		; when ecx is 0 we continue, else goto Loop
-
+	xor eax,eax			; clear eax
+	mov edx,ecx			; store offset in edx
+	
+	mov al,byte [esi+edx]	; get byte from buffer with offset
+	and al,1Fh			; mask out the 5 first bits 0001 1111
+	mov bl,byte [Table+al]	; get the matching character from data Table
+	mov byte [Res+edx],bl	; store this character in memory (Res)
+	
+	add ecx,r5			; shift to the next 5 bits
+	cmp ecx,ebp			; compare pointer to buffer
+	jna Loop			; 
+	
 ; Write the line of hexadecimal values to stdout:
 	mov eax,Buff		; Specify sys_write call
-	mov ebx,ebp		; Specify File Descriptor 1: Standard output
+	mov ebx,ebp			; Specify File Descriptor 1: Standard output
 	call PrintString	; call PrintString from io.asm
-	jmp Read		; Loop back and load file buffer again
+	jmp Read			; Loop back and load file buffer again
 
 ; All done! Let's end this party:
 Done:
