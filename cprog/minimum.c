e86f9b6f76e830d4467f388b24e0028f8c1d5756
Janick <stuckijanick@gmail.com>
2018-12-03 17:46:57 +0100

    added some more c programs

---
+++ cprog/minimum.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int minimum(int *a, int size);
+void minimax(int *array, int size, int *min, int *max);
+int main() {
+	int array[]={34,54,2,43,78};
+	int mini = minimum(array,5);
+	printf("mini = %d\n", mini);
+	int min;
+	int max;
+	minimax(array,5,&min,&max);
+	printf("min = %d  max = %d\n", min, max);
+	return 0;
+}
+
+int minimum(int *a, int size) {
+	int ret = *a; // start with first number
+	for(int i = 1; i < size; i++) { // loop and see if there are smaller numbers
+		if (*(a+i) < ret) {
+			ret = *(a+i);
+		}
+	}
+	return ret;
+}
+
+void minimax(int *array, int size, int *min, int *max) {
+	*min = *array;
+	*max = *array;
+	for(int i = 1; i < size; i++) { // loop and see if there are smaller numbers
+		if (*(array+i) < *min) {
+			*min = *array;
+		}
+		if (*(array+i) > *max) {
+			*max = *array;
+		}
+	}
+}
+
