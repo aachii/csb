2eda54346506edb3e447b5db559937ac7c8798d4
janick <stuckijanick@gmail.com>
2019-01-16 07:39:01 +0100

    have correct data but wrong formatted

--- ps/ps.c
+++ ps/ps.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 	int pid_max; // variable for max pid count
 	FILE *f_pid_max; // read pid_max from file
 	FILE *f_pid; // variable for pid in loop
 	char pid_path[64]; // for path to pid status file
-	char pid_name[64]; // name from status file
+	char buff[64]; // buffer for status fule
+	char pid_name[64]; // name
 	int pid_uid; // uid
 	int pid_vmrss; // memory
 	f_pid_max = fopen("/proc/sys/kernel/pid_max", "r");
@@ -17,9 +19,19 @@ int main() {
 	for (int pid=0; pid<=pid_max; pid++) {
 		sprintf(pid_path, "/proc/%d/status", pid);
 		if (f_pid = fopen(pid_path, "r")) {
-			fscanf(f_pid, "Name: %s", pid_name);
-			fscanf(f_pid, "Uid: %d", &pid_uid);
-			fscanf(f_pid, "VmRSS: %d", &pid_vmrss);
+			while (fgets(buff, sizeof(buff), f_pid) != NULL) {
+				buff[strlen(buff)-1] = '\0'; // remove newline at end
+				if (strstr(buff, "Name")) {
+					printf("%s\n", buff);
+					//fscanf(f_pid, "Name: %s", pid_name);
+				} else if (strstr(buff, "Uid")) {
+					printf("%s\n", buff);
+					//fscanf(f_pid, "Uid: %d ", &pid_uid);
+				} else if (strstr(buff, "VmRSS")) {
+					printf("%s\n", buff);
+					//fscanf(f_pid, "VmRSS: %d ", &pid_vmrss);
+				}
+			}
 			printf("%d %s %d %d\n", pid, pid_name, pid_uid, pid_vmrss);
 
 			//printf("pid %d valid\n", pid);
